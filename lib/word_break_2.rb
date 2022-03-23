84bd5e055c6475d206c382875a591688ea86c0be
Steve Whitney <steve.whitney@gmail.com>
2022-03-23 17:00:44 +0000

    remove-part-sol

--- lib/word_break_2.rb
+++ lib/word_break_2.rb
@@ -40,36 +40,3 @@ class WordBreak2
     "#{sentence_accumulator}#{spacer}#{new_word}"
   end
 end
-
-# # Interpolate a string with spaces to form sentences according to a dictionary.
-# # https://leetcode.com/problems/word-break-ii/
-# class WordBreak2
-#   def word_break(original_str, dict)
-#     dictionary = Set.new
-#     dict.each { |entry| dict << entry.downcase }
-
-#     result_accumulator = []
-#     find_words(dictionary, original_str, 0, '', result_accumulator)
-#     result_accumulator
-#   end
-
-#   private
-
-#   def find_words(dictionary, original_str, start_index, sentence_accumulator, result_accumulator)
-#     i = start_index
-#     while i <= original_str.size
-#       new_word = str[start_index, i - start_index].downcase
-#       is_word = dictionary.include?(new_word)
-
-#       if is_word && i >= original_str.size
-#         sentence_accumulator << increase_sentence_accumulator(sentence_accumulator, new_word)
-#       elsif is_word
-#         find_words(dictionary, original_str, i, increase_sentence_accumulator(sentence_accumulator, new_word),
-#                    result_accumulator)
-#       end
-
-#       i += 1
-#     end
-#   end
-
-# end
